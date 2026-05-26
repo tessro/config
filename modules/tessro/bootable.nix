346e5cf705abb060171d22326e66a287bcdd2d29
Tess Rosania <tess.rosania@gmail.com>
2026-05-25 23:27:52 -0700

    add sshid.io keys

--- modules/tessro/bootable.nix
+++ modules/tessro/bootable.nix
@@ -24,6 +24,11 @@
 
       users.users.root.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgixMp9vffZXwMH/oHanhXgis2yn6xLhA4vUPR4924N"
+
+        # sshid.io/tessro
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDS6fhFpU3zjYkFfVDtgGEhZYPZPcguinnn/fhq7EA7r"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+SmQ/0M3NZJ5rBAW/82JAGcwIjLdo0EYAVz6CKs3b1"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIj76DVPyrEs1z69PWwxudQWYYSTqjoSNMi4NVlDr1kY"
       ];
     };
   };
