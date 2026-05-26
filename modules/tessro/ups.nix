39e30e8ae1eb50edd6f4a78f7350b1a92e5413f0
Tess Rosania <tess.rosania@gmail.com>
2026-05-26 08:19:19 -0700

    upsmon

--- modules/tessro/ups.nix
+++ modules/tessro/ups.nix
@@ -1,21 +1,59 @@
 { tessro, ... }: {
   tessro.ups = {
-    nixos = { pkgs, ... }: {
-      environment.systemPackages = with pkgs; [
-        nut
-      ];
+    nixos = { pkgs, ... }:
 
-      power.ups = {
-        enable = true;
-        mode = "standalone";
+      let
+        upsmonPasswordFile = "/var/lib/secrets/nut/upsmon-password";
+      in
+      {
+        environment.systemPackages = with pkgs; [
+          nut
+        ];
 
-        ups.main = {
-          driver = "usbhid-ups";
-          port = "auto";
+        system.activationScripts.generateNutUpsmonPassword = ''
+          set -eu
+
+          if [ ! -s "${upsmonPasswordFile}" ]; then
+            ${pkgs.coreutils}/bin/install -d -m 0700 -o root -g root /var/lib/secrets/nut
+
+            tmp="$(${pkgs.coreutils}/bin/mktemp "${upsmonPasswordFile}.XXXXXX")"
+
+            ${pkgs.coreutils}/bin/head -c 32 /dev/urandom \
+              | ${pkgs.coreutils}/bin/base64 -w0 \
+              > "$tmp"
+
+            ${pkgs.coreutils}/bin/chown root:root "$tmp"
+            ${pkgs.coreutils}/bin/chmod 0400 "$tmp"
+            ${pkgs.coreutils}/bin/mv -f "$tmp" "${upsmonPasswordFile}"
+          fi
+        '';
+
+        power.ups = {
+          enable = true;
+          mode = "standalone";
+
+          ups.main = {
+            driver = "usbhid-ups";
+            port = "auto";
+          };
+
+          users.upsmon = {
+            passwordFile = upsmonPasswordFile;
+            upsmon = "primary";
+          };
+
+          upsmon = {
+            enable = true;
+
+            monitor.main = {
+              system = "main@localhost";
+              powerValue = 1;
+              user = "upsmon";
+              passwordFile = upsmonPasswordFile;
+              type = "primary";
+            };
+          };
         };
-
-        upsmon.enable = false;
       };
-    };
   };
 }
