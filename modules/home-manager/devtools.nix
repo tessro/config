db4f568342436706a8832cfe7e9c2d74a7a09207
Tess Rosania <tess.rosania@gmail.com>
2026-06-16 23:02:16 +0200

    keep gcc + gnumake out of darwin

--- modules/home-manager/devtools.nix
+++ modules/home-manager/devtools.nix
@@ -1,34 +1,38 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    btop
-    cmake
-    curl
-    devenv
-    fd
-    fzf
-    gcc
-    gh
-    git
-    git-lfs
-    gnumake
-    go
-    jq
-    neovim
-    packer
-    python3
-    ripgrep
-    wget
+  home.packages =
+    with pkgs;
+    [
+      btop
+      cmake
+      curl
+      devenv
+      fd
+      fzf
+      gh
+      git
+      git-lfs
+      go
+      jq
+      neovim
+      packer
+      python3
+      ripgrep
+      wget
 
-    # infra
-    awscli2
-    opentofu
+      # infra
+      awscli2
+      opentofu
 
-    # lsp
-    gopls
-    lua-language-server
-    tofu-ls
-  ];
+      # lsp
+      gopls
+      lua-language-server
+      tofu-ls
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      gcc
+      gnumake
+    ];
 
   programs.java = {
     enable = true;
