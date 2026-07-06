96405508a6a171ad36a64cabe8653ed7113d9e33
Tess Rosania <tess@sierra.ai>
2026-07-06 14:24:52 -0700

    Add work SSH host settings

--- hosts/tess-at-work.nix
+++ hosts/tess-at-work.nix
@@ -8,6 +8,23 @@
   tess.homebrew.includePersonal = false;
   tess.homebrew.includeCore = true;
 
+  home-manager.users.tess.programs.ssh.extraConfig = lib.mkAfter ''
+    Host cloud-2
+      ForwardAgent yes
+      SetEnv TERM=xterm-256color
+
+    # Staging has to be first or prod will match it.
+    Host *.staging.pinecone.internal
+      User runner
+      Port 2222
+      ProxyJump bastion@ssh.staging.pinecone.cloud
+
+    Host *.pinecone.internal
+      User runner
+      Port 2222
+      ProxyJump bastion@ssh.pinecone.cloud
+  '';
+
   nix-homebrew.autoMigrate = true;
 
   system.stateVersion = 6;
