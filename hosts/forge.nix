e56cbc91f8ddad47caa04a909ad62e6d618defc2
Tess Rosania <tess.rosania@gmail.com>
2026-07-14 20:36:59 -0700

    forge: fix module imports

--- hosts/forge.nix
+++ hosts/forge.nix
@@ -1,17 +1,15 @@
-{ modulesPath, ... }:
+{ lib, modulesPath, ... }:
 {
   imports = [
     "${toString modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
     ../modules/nixos
-    ../modules/core.nix
-    ../modules/bootable.nix
-    ../modules/nix.nix
+    ../modules/nixos/bootable.nix
     ../modules/users/tess.nix
   ];
 
   system.stateVersion = "26.11";
 
-  services.openssh.settings.PermitRootLogin = "prohibit-password";
+  services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
 
   networking.networkmanager.enable = true;
 }
