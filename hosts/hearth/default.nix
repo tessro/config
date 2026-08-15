97c092deaf94328995baa08f80180e017ec69b6f
Tess Rosania <tess.rosania@gmail.com>
2026-08-15 12:38:27 -0700

    hearth: add hardware diagnostics

--- hosts/hearth/default.nix
+++ hosts/hearth/default.nix
@@ -103,4 +103,6 @@ in
 
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;
+
+  hardware.rasdaemon.enable = true;
 }
--- modules/nixos/bootable.nix
+++ modules/nixos/bootable.nix
@@ -6,6 +6,8 @@
   ];
 
   environment.systemPackages = with pkgs; [
+    dmidecode
+    ipmitool
     pciutils
     usbutils
     nvme-cli
